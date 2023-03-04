33eba633ab6af897aad5525d884e48ede3de4a0e
김연중Home-PC <illulizer@gmail.com>
2023-03-04 17:10:05 +0900

    cleanup

---
+++ CRA/cleanup/src/App.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  function destroyedFn() {
+    console.log('destroyed');
+  }
+  function effectFn() {
+    console.log("created");
+    return destroyedFn;
+  }
+
+  useEffect(effectFn, [])
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev)
+
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
